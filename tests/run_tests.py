04f3e7ab1e61ad156e06a6b36d36b3e8cef0d64c
niniack <nsa325@nyu.edu>
2023-10-01 21:21:05 +0400

    Init testing with pytest

---
+++ tests/run_tests.py
@@ -0,0 +1,32 @@
+import sys
+from pathlib import Path
+
+import pytest
+
+
+def main():
+    if len(sys.argv) < 2:
+        print(
+            "Please specify a test file or a directory relative to the 'tests' directory"
+        )
+        sys.exit(1)
+
+    # Split the input to extract the file and the test part separately.
+    parts = sys.argv[1].split("::")
+    file_part = parts[0]
+
+    # Constructing the absolute path to the file, relative to the script's directory.
+    script_dir = Path(__file__).parent
+    test_file_path = script_dir / file_part
+
+    if not test_file_path.exists():
+        print(f"ERROR: The file {test_file_path} does not exist.")
+        sys.exit(1)
+
+    # Construct the argument for pytest.main by combining the absolute path of the test file
+    # with the test name specifier, if it was provided.
+    pytest_arg = str(test_file_path)
+    if len(parts) > 1:
+        pytest_arg += "::" + parts[1]
+
+    sys.exit(pytest.main([pytest_arg]))
--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,37 +1,28 @@
-import unittest
-from pathlib import Path
-
 import numpy as np
-import torch
 from torch.testing import assert_close
 
 from continualUtils.benchmarks.datasets.preprocess import preprocess_input
 
 
-class TestNormalizeDataset(unittest.TestCase):
-    def test_normalize_np_image(self):
-        # Create a 3D tensor
-        np_img = np.array(
-            [
-                [[255.0, 255.0, 255.0], [255.0, 255.0, 255.0]],
-                [[255.0, 255.0, 255.0], [255.0, 255.0, 255.0]],
-            ]
-        )
+def test_normalize_np_image():
+    # Create a 3D tensor
+    np_img = np.array(
+        [
+            [[255.0, 255.0, 255.0], [255.0, 255.0, 255.0]],
+            [[255.0, 255.0, 255.0], [255.0, 255.0, 255.0]],
+        ]
+    )
 
-        # Call the normalization function
-        normalized_img = preprocess_input(np_img)
+    # Call the normalization function
+    normalized_img = preprocess_input(np_img)
 
-        # Define expectation
-        expected_img = np.array(
-            [
-                [[2.249, 2.429, 2.640], [2.249, 2.429, 2.640]],
-                [[2.249, 2.429, 2.640], [2.249, 2.429, 2.640]],
-            ]
-        )
+    # Define expectation
+    expected_img = np.array(
+        [
+            [[2.249, 2.429, 2.640], [2.249, 2.429, 2.640]],
+            [[2.249, 2.429, 2.640], [2.249, 2.429, 2.640]],
+        ]
+    )
 
-        # Assert that the output is as expected
-        assert_close(normalized_img, expected_img, rtol=1e-03, atol=1e-03)
-
-
-if __name__ == "__main__":
-    unittest.main()
+    # Assert that the output is as expected
+    assert_close(normalized_img, expected_img, rtol=1e-03, atol=1e-03)
