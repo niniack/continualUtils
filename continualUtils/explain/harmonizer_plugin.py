1d37978353e74d32ce1a219960e91ab7f61e1ce2
niniack <nsa325@nyu.edu>
2023-10-30 16:35:57 +0000

    detach grad for clarity and disable eval stuff

--- continualUtils/explain/harmonizer_plugin.py
+++ continualUtils/explain/harmonizer_plugin.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 import torch
 from avalanche.training.plugins.strategy_plugin import SupervisedPlugin
 
@@ -56,82 +58,81 @@ class NeuralHarmonizerPlugin(SupervisedPlugin):
         self.weight = weight
         self.harmonizer = NeuralHarmonizerLoss(weight)
 
-    def before_backward(self, strategy, *args, **kwargs):
+    def before_forward(self, strategy: Any, *args, **kwargs):
+        # Clean up harmonizer loss
+        strategy.harmonizer_loss = torch.zeros(1, device=strategy.device)
+
+    def before_backward(self, strategy: Any, *args, **kwargs):
+        # Clone the input
         cloned_mb_x = strategy.mb_x.detach()
 
+        # Turn on gradients for the cloned input
         if cloned_mb_x.requires_grad is False:
             cloned_mb_x.requires_grad_(True)
 
         # Get the heatmaps and tokens
-        strategy.mb_heatmap = strategy.mbatch[HEATMAP_INDEX]
+        strategy.mb_heatmaps = strategy.mbatch[HEATMAP_INDEX]
         strategy.mb_tokens = strategy.mbatch[TOKEN_INDEX]
         strategy.mb_tasks = strategy.mbatch[4]
 
         # Uses the `__call__` method
+        # Send in the cloned input with gradients turned on
         harmonizer_loss = self.harmonizer(
             cloned_mb_x,
             strategy.mb_y,
-            strategy.mb_heatmap,
+            strategy.mb_heatmaps,
             strategy.model,
             strategy.mb_tokens,
             strategy.mb_tasks,
         )
 
+        # Add the harmonizer loss
         strategy.loss += harmonizer_loss
         strategy.harmonizer_loss = harmonizer_loss
 
-        # if torch.is_tensor(harmonizer_loss):
-        #     harmonizer_loss = harmonizer_loss.cpu().item()
-
-        # step = strategy.clock.total_iterations
-        # exp_counter = strategy.clock.train_exp_counter
-        # mname = "harmonizer_loss/" + "exp" + str(exp_counter)
-        # mval = MetricValue(self, mname, harmonizer_loss, step)
-        # strategy.evaluator.publish_metric_value(mval)
-
-    # # TODO: is this relevant?
-    # def after_training_exp(self, strategy, **kwargs):
-    #     self.harmonizer.update()
-
-    def before_eval_exp(self, strategy, *args, **kwargs):
-        strategy.harmonizer_loss = 0
-
-    def before_eval_forward(self, strategy, *args, **kwargs):
-        cloned_mb_x = strategy.mb_x.detach()
-
-        if not cloned_mb_x.requires_grad:
-            cloned_mb_x.requires_grad_(True)
-
-        # Compute output and heatmap
-        with torch.enable_grad():
-            # Get the heatmaps and tokens
-            strategy.mb_heatmap = strategy.mbatch[HEATMAP_INDEX]
-            strategy.mb_tokens = strategy.mbatch[TOKEN_INDEX]
-
-            # Uses the `__call__` method
-            harmonizer_loss = self.harmonizer(
-                cloned_mb_x,
-                strategy.mb_y,
-                strategy.mb_heatmap,
-                strategy.model,
-                strategy.mb_tokens,
-            )
-
-        # Disable gradient on input
+        # Turn off gradients for the cloned input
         cloned_mb_x.requires_grad_(False)
         cloned_mb_x.detach()
 
-        # Set loss
-        strategy.mb_harmonizer_loss = harmonizer_loss
-        strategy.harmonizer_loss += harmonizer_loss
+    # def before_eval_exp(self, strategy, *args, **kwargs):
+    #     strategy.harmonizer_loss = 0
 
-        if torch.is_tensor(harmonizer_loss):
-            harmonizer_loss = harmonizer_loss.cpu().item()
+    # def before_eval_forward(self, strategy, *args, **kwargs):
+    #     cloned_mb_x = strategy.mb_x.detach()
 
-    def after_eval_exp(self, strategy, *args, **kwargs):
-        num_eval_samples = len(strategy.dataloader.dataset)
-        strategy.harmonizer_loss /= num_eval_samples
+    #     if not cloned_mb_x.requires_grad:
+    #         cloned_mb_x.requires_grad_(True)
 
-    # Avalanche defines loss *just* before this callback
-    def after_eval_iteration(self, strategy, *args, **kwargs):
-        strategy.loss += strategy.mb_harmonizer_loss
+    #     # Compute output and heatmap
+    #     with torch.enable_grad():
+    #         # Get the heatmaps and tokens
+    #         strategy.mb_heatmap = strategy.mbatch[HEATMAP_INDEX]
+    #         strategy.mb_tokens = strategy.mbatch[TOKEN_INDEX]
+
+    #         # Uses the `__call__` method
+    #         harmonizer_loss = self.harmonizer(
+    #             cloned_mb_x,
+    #             strategy.mb_y,
+    #             strategy.mb_heatmap,
+    #             strategy.model,
+    #             strategy.mb_tokens,
+    #         )
+
+    #     # Disable gradient on input
+    #     cloned_mb_x.requires_grad_(False)
+    #     cloned_mb_x.detach()
+
+    #     # Set loss
+    #     strategy.mb_harmonizer_loss = harmonizer_loss
+    #     strategy.harmonizer_loss += harmonizer_loss
+
+    #     if torch.is_tensor(harmonizer_loss):
+    #         harmonizer_loss = harmonizer_loss.cpu().item()
+
+    # def after_eval_exp(self, strategy, *args, **kwargs):
+    #     num_eval_samples = len(strategy.dataloader.dataset)
+    #     strategy.harmonizer_loss /= num_eval_samples
+
+    # # Avalanche defines loss *just* before this callback
+    # def after_eval_iteration(self, strategy, *args, **kwargs):
+    #     strategy.loss += strategy.mb_harmonizer_loss
