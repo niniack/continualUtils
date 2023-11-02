8ebd7be1780bbb359a66e8b9d903fdc78796bfb8
niniack <nsa325@nyu.edu>
2023-11-02 09:16:25 +0000

    Visualize saliency maps in notebook

--- continualUtils/evaluation/saliency_map_metrics.py
+++ continualUtils/evaluation/saliency_map_metrics.py
@@ -88,8 +88,8 @@ class SaliencyMapSamplePlugin(PluginMetric):
         self._load_sorted_images(strategy, experience)
 
         # Rescale the heatmaps between 0 and 1
-        rescaled_images = self._rescale_batch(self.images).cpu()
-        rescaled_maps = self._rescale_batch(self.computed_maps).cpu()
+        rescaled_images = rescale_batch(self.images).cpu()
+        rescaled_maps = rescale_batch(self.computed_maps).cpu()
 
         # Overlay the heatmaps
         overlayed_images = show_cam_on_image(
@@ -212,40 +212,6 @@ class SaliencyMapSamplePlugin(PluginMetric):
             collate_fn=collate_fn,
         )
 
-    def _rescale_batch(self, inputs: Union[List[Tensor], Tensor]) -> Tensor:
-        """Rescale a NCHW batch from 0 to 1
-
-        :param inputs: A list of 3D CHW tensors or a 4D tensor in NCHW
-        :return: NCHW tensor scaled from 0 to 1
-        """
-        # Handle list of tensors
-        if isinstance(inputs, list):
-            # Stack the list of tensors to form a 4D tensor
-            inputs = torch.stack(inputs, dim=0)
-
-        # Rest of your logic remains unchanged
-
-        # Get dims
-        n, c, h, w = inputs.shape
-
-        # Flatten CHW dims in view
-        inputs_flattened = inputs.view(inputs.size(0), -1)
-
-        min_vals = inputs_flattened.min(dim=1, keepdim=True).values
-        max_vals = inputs_flattened.max(dim=1, keepdim=True).values
-
-        # Reshape the min and max values to BCHW
-        min_vals = min_vals.view(n, 1, 1, 1).expand(n, c, h, w)
-        max_vals = max_vals.view(n, 1, 1, 1).expand(n, c, h, w)
-
-        # Rescale between 0 and 1
-        # Added epsilon to avoid division by zero
-        inputs = (inputs - min_vals) / (max_vals - min_vals + 1e-15)
-
-        inputs = inputs.to(torch.float32)  # type: ignore
-
-        return inputs
-
     def _compute_maps(
         self, images: Tensor, labels: Tensor, strategy: "SupervisedTemplate"
     ) -> Tensor:
@@ -262,6 +228,39 @@ class SaliencyMapSamplePlugin(PluginMetric):
             create_graph=False,
         )
         return computed_maps.detach()
+
+
+def rescale_batch(inputs: Union[List[Tensor], Tensor]) -> Tensor:
+    """Rescale a NCHW batch from 0 to 1
+
+    :param inputs: A list of 3D CHW tensors or a 4D tensor in NCHW
+    :return: NCHW tensor scaled from 0 to 1
+    """
+    # Handle list of tensors
+    if isinstance(inputs, list):
+        # Stack the list of tensors to form a 4D tensor
+        inputs = torch.stack(inputs, dim=0)
+
+    # Get dims
+    n, c, h, w = inputs.shape
+
+    # Flatten CHW dims in view
+    inputs_flattened = inputs.view(inputs.size(0), -1)
+
+    min_vals = inputs_flattened.min(dim=1, keepdim=True).values
+    max_vals = inputs_flattened.max(dim=1, keepdim=True).values
+
+    # Reshape the min and max values to BCHW
+    min_vals = min_vals.view(n, 1, 1, 1).expand(n, c, h, w)
+    max_vals = max_vals.view(n, 1, 1, 1).expand(n, c, h, w)
+
+    # Rescale between 0 and 1
+    # Added epsilon to avoid division by zero
+    inputs = (inputs - min_vals) / (max_vals - min_vals + 1e-15)
+
+    inputs = inputs.to(torch.float32)  # type: ignore
+
+    return inputs
 
 
 def show_cam_on_image(
