4dc244d3c6e63ce8115c831f93675802b13bb601
niniack <nsa325@nyu.edu>
2023-11-06 12:24:59 +0000

    Include resnet34 in init

--- continualUtils/models/__init__.py
+++ continualUtils/models/__init__.py
@@ -1,4 +1,5 @@
 from continualUtils.models.base import *
 from continualUtils.models.cnn import *
 from continualUtils.models.resnet18 import *
+from continualUtils.models.resnet34 import *
 from continualUtils.models.resnet50 import *
